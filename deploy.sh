9f7b9383d75cdb5bb0a98feb683654e25ce5c203
Eric Duffy <eric@eduff.net>
2015-09-26 08:20:10 -0400

    Adding a simple deployment script

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+
+cp index.py ../www/index.py
+cp pages/travel.py ../www/pages/travel.py
+cp pages/webgen.py ../www/pages/webgen.py
+cp pages/books.py ../www/pages/books.py
+cp pages/courses.py ../www/pages/courses.py
+cp pages/music.py ../www/pages/music.py
+cp pages/projects.py ../www/pages/projects.py
