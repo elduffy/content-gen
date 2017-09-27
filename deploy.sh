6f2ddf66234778b43265ddaaa805b2d1e4c9dab2
Eric Duffy <eric@eduff.net>
2017-09-27 10:35:44 -0500

    Also deploy generation scripts.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 
+cp WebGen.py ../www/WebGen.py
+cp ContentGen.py ../www/ContentGen.py
 cp index.py ../www/index.py
 cp pages/travel.py ../www/pages/travel.py
 cp pages/courses.py ../www/pages/courses.py
